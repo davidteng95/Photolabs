2a93616e28afd21abaabee6146aa46f648bffb4a
davidteng95 <davidteng95@gmail.com>
2023-07-14 08:24:08 -0600

    import navigation bar and photolist

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+import topics from '../mocks/topics';
+import photos from '../mocks/photos';
 
-const HomeRoute = () => 
-  <div className="home-route">
-    {/* Insert React */}
-  </div>
+const HomeRoute = () => {
+  const [favourites, setFavourites] = useState([]);
+
+  return (
+    <div className="home-route">
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos} />
+    </div>
+  );
+};
 
 export default HomeRoute;
