c5ec384c1935987620b5a8832fe029de84138964
davidteng95 <davidteng95@gmail.com>
2023-07-14 08:22:05 -0600

    configure photos, add css and fav button

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,36 @@
 import React from 'react';
 
 import '../styles/PhotoListItem.scss';
+import PhotoFavButton from './PhotoFavButton';
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
+  const {photo} = props;
+  console.log(props);
   /* Insert React */
-}
+  /*<div className='photo-details'>
+        <div className='username'>{props.username}</div>
+        <div className='location'>{props.location.city}, {props.location.country}</div>
+      </div>
+      <img src={props.profile} alt={`${props.username}'s profile`} className='profile-picture' />
+      */
+  return (
+    <div className='photo-list__item'>
+      <PhotoFavButton />
+      <img src={photo.urls.regular} alt={`Photo ${photo.id}`} className='photo-list__image' />
+      
+    </div>
+  );
+};
 
-PhotoListItem.defaultProps = {
-  "id": "1",
-  "location": {
-    "city": "Montreal",
-    "country": "Canada"
-  },
-  "imageSource": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  "username": "Joe Example",
-  "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-}
+// PhotoListItem.defaultProps = {
+//   "id": "1",
+//   "location": {
+//     "city": "Montreal",
+//     "country": "Canada"
+//   },
+//   "imageSource": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   "username": "Joe Example",
+//   "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
+// };
 
-export default PhotoListItem
+export default PhotoListItem;
