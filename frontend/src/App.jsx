618f15b35ec77324d6cca42fb1f9629ff8b2109c
davidteng95 <davidteng95@gmail.com>
2023-07-14 08:25:05 -0600

    add HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import React from "react";
+// import PhotoList from "./components/PhotoList";
+// import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
+// import PhotoFavButton from "./components/PhotoFavButton";
+// import TopicList from "./components/TopicList";
+// import topics from "./mocks/topics";
+// import photos from "./mocks/photos";
+// import TopNavigationBar from "./components/TopNavigationBar";
+import HomeRoute from "./routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+const App = () => {
+  // const photos = new Array(3).fill("");
 
-export default App
+  return (
+    <div className="App">
+      <HomeRoute />
+      {/* <TopNavigationBar /> */}
+      {/* <TopicList topics={topics} /> */}
+      {/* <span className="photo-list">
+        {photos.map((photo, index) => (<PhotoListItem key={index} photo={photo} />))}
+      </span> */}
+      {/* <PhotoList /> */}
+    </div>
+  );
+};
+
+export default App;
