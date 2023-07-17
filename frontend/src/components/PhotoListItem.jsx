d3367e2cca2959cb4b40c0e7e0ebeb7d28c3dbf3
davidteng95 <davidteng95@gmail.com>
2023-07-17 16:13:39 -0600

    implement modal

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,12 @@
+import React from "react";
 
-import React from 'react';
-
-import '../styles/PhotoListItem.scss';
-import PhotoFavButton from './PhotoFavButton';
+import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const {photo, favourites, toggleFavourite} = props;
-  console.log(props);
+  const { photo, favourites, toggleFavourite, onPhotoSelect } = props;
+  // console.log("photolistitem", photo);
+  // console.log(props);
   /* Insert React */
   /*<div className='photo-details'>
         <div className='username'>{props.username}</div>
@@ -15,10 +15,27 @@ const PhotoListItem = (props) => {
       <img src={props.profile} alt={`${props.username}'s profile`} className='profile-picture' />
       */
   return (
-    <div className='photo-list__item'>
-      <PhotoFavButton favourites={favourites} toggleFavourite={toggleFavourite} photoId={photo.id}/>
-      <img src={photo.urls.regular} alt={`Photo ${photo.id}`} className='photo-list__image' />
-      
+    <div className="photo-list__item">
+      <PhotoFavButton
+        favourites={favourites}
+        toggleFavourite={toggleFavourite}
+        photoId={photo.id}
+      />
+      <img
+        src={photo.urls.regular}
+        alt={`Photo ${photo.id}`}
+        className="photo-list__image"
+        onClick={() => {
+          onPhotoSelect(photo);
+        }}
+      />
+      <div className="photo-list__user-details">
+        <img src={photo.user.profile} className="photo-list__user-profile"/>
+        <div className="photo-list__user-info-box">
+          <div className="photo-list__user-info">{photo.user.name}</div>
+          <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/PhotoDetailsModal.scss'
+import "../styles/PhotoDetailsModal.scss";
 
-export const PhotoDetailsModal = () => (
-  <div className='photo-details-modal'>
-    <button className='photo-details-modal--close-button'>
-      <svg width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clipPath="url(#clip0_428_287)">
-          <path d="M14.0625 3.9375L3.9375 14.0625" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M14.0625 14.0625L3.9375 3.9375" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
-        </g>
-        <defs>
-          <clipPath id="clip0_428_287">
-          <rect width="18" height="18" fill="white"/>
-          </clipPath>
-        </defs>
-      </svg>
-    </button>
-  </div>
-)
+import PhotoList from "../components/PhotoList";
+import PhotoFavButton from "../components/PhotoFavButton";
 
+const PhotoDetailsModal = (props) => {
+  const { onClose, photo, favourites, toggleFavourite } = props;
+  console.log("detail", photo);
+  return (
+    <div className="photo-details-modal">
+      <button className="photo-details-modal__close-button" onClick={onClose}>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clipPath="url(#clip0_428_287)">
+            <path
+              d="M14.0625 3.9375L3.9375 14.0625"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M14.0625 14.0625L3.9375 3.9375"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_428_287">
+              <rect width="18" height="18" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </button>
+
+      <div className="photo-details-modal__images">
+        <PhotoFavButton favourites={favourites}
+          toggleFavourite={toggleFavourite}/>
+        <img src={photo.urls.full} className="photo-details-modal__image" />
+        <div className="photo-list__user-details">
+          <img src={photo.user.profile} className="photo-list__user-profile" />
+          <div className="photo-list__user-info-box">
+            <div className="photo-list__user-info">{photo.user.name}</div>
+            <div className="photo-list__user-location">
+              {photo.location.city}, {photo.location.country}
+            </div>
+          </div>
+        </div>
+        <p className="photo-details-modal__header">Similar Photos</p>
+        <PhotoList
+          photos={Object.values(photo.similarPhotos) || []}
+          favourites={favourites}
+          toggleFavourite={toggleFavourite}
+        />
+      </div>
+    </div>
+  );
+};
 export default PhotoDetailsModal;
