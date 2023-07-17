8b12c1570761857b61fa10c78b7f0e6fc96198b5
davidteng95 <davidteng95@gmail.com>
2023-07-17 14:54:10 -0600

    implement useReducer

---
+++ frontend/src/hooks/userApplicationData.js
@@ -0,0 +1,67 @@
+import React, { useReducer } from 'react';
+
+const initialState = {
+  favourites: [],
+  showModal: false,
+  selectedPhoto: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case 'TOGGLE_FAVOURITES':
+    if (state.favourites.includes(action.id)) {
+      //already included
+      return {
+        ...state,
+        favourites: state.favourites.filter((photo) => photo !== action.id)
+      };
+    }
+      
+    return {
+      ...state,
+      favourites: [...state.favourites, action.id],
+    };
+  case 'SET_PHOTO_CLICKED':
+    return {
+      ...state,
+      showModal: action.clicked,
+    };
+  case 'SELECT_PHOTO':
+    return {
+      ...state,
+      selectedPhoto: action.info,
+    };
+  default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
+  }
+};
+
+const useApplicationData = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleFavourites = (id) => {
+    dispatch({ type: 'TOGGLE_FAVOURITES', id });
+  };
+
+  const setShowModal = (clicked) => {
+    dispatch({ type: 'SET_PHOTO_CLICKED', clicked });
+  };
+
+  const setSelectedPhoto = (info) => {
+    dispatch({ type: 'SELECT_PHOTO', info });
+  };
+
+  return {
+    state: state,
+    toggleFavourites,
+    setShowModal,
+    setSelectedPhoto
+  };
+
+
+};
+
+export default useApplicationData;
